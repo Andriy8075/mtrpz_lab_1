d4d6db027b2980749865a66410d0e5b86f20e360
andriy8075 <andriy8075@gmail.com>
2025-02-28 11:58:07 +0200

    interactive

---
+++ interactive.py
@@ -0,0 +1,23 @@
+from general_script import solve_quadratic_equation
+
+def validate_input(prompt):
+    try:
+        replaced_commas = prompt.replace(',', '.')
+        return float(replaced_commas)
+    except ValueError:
+        print(f"Error. Expected a valid real number, got \"{prompt}\" instead")
+        exit(1)
+
+def main():
+    a = validate_input(input("a = "))
+    if a == 0:
+        print("a can not be 0")
+        return
+    b = validate_input(input("b = "))
+    c = validate_input(input("c = "))
+
+    print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+    solve_quadratic_equation(a, b, c)
+
+if __name__ == "__main__":
+    main()
